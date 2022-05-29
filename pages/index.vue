e49262672656c69d824cec98dd51152384bb8c19
andrikosbo <105873154+andrikosbo@users.noreply.github.com>
2022-05-29 18:34:31 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@
       >
         <div
           class="logo w-16 m-4"
-          v-html="require(`~/assets/icons/logo2.svg?include`)"
+          v-html="require(`~/assets/icons/logo.svg?include`)"
         ></div>
         <button
           class="
@@ -72,7 +72,7 @@
       <div class="flex items-start justify-between pt-8">
         <div
           class="logo w-24"
-          v-html="require(`~/assets/icons/logo.svg?include`)"
+          v-html="require(`~/assets/icons/logo2.svg?include`)"
           title="Bombinate.GR - Digital Business Card Generator"
         ></div>
        
