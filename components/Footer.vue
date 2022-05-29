6de7fabfcd37900f70ecfa4a45e09141c5bb415c
andrikosbo <105873154+andrikosbo@users.noreply.github.com>
2022-05-29 18:57:45 +0300

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,7 +10,7 @@
         <br />
         Generator
       </p> -->
-      <p class="font-extrabold text-xl mt-2 leading-tight">EnBizCard</p>
+      <p class="font-extrabold text-xl mt-2 leading-tight">Bombinate.GR DBC</p>
     </div>
     <p class="mt-6 text-center text-gray-400">
       Powered ❤️ by
